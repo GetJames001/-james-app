fd6fbe0da20c668fe65c92452a13aae594af571d
GetJames001 <allproactive@hotmail.com>
2026-08-21 14:44:23 -0700

    Fix typo in CORS header setting

--- api/google-search-test.js
+++ api/google-search-test.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");c
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
